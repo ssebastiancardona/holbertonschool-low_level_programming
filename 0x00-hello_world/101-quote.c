0b4ea7cec8a94bd7f785a25e506797dcd6d30093
ssebastiancardona <ssebastiancardona@gmail.com>
2021-02-03 23:55:34 -0500

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entra al programa principal
+ *
+ * Return: En esta ocasion retorna 1
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
