3adf1fb2692f9d96edd2bcb3cb2ebd6222dae374
ssebastiancardona <ssebastiancardona@gmail.com>
2021-02-05 18:26:37 -0500

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entra en el programa principal
+ *
+ * Return: Retorna 0
+ */
+int main(void)
+{
+char l;
+l = 'a';
+while (l <= 'z')
+{
+putchar(l);
+l++;
+}
+putchar('\n');
+return (0);
+}
