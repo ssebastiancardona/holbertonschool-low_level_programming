1350d400bb5a0d96b717cd860430f861e3778c70
ssebastiancardona <ssebastiancardona@gmail.com>
2021-02-05 20:40:49 -0500

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entra al programa principal
+ *
+ * Return: Retorna 0
+ */
+int main(void)
+{
+int i;
+i = 48;
+while  (i < 58)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
