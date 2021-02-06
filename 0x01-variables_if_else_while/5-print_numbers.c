741683c458d3d4fa7424654ef329a755430a6b72
ssebastiancardona <ssebastiancardona@gmail.com>
2021-02-05 20:39:01 -0500

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entra al programa principal
+ *
+ * Return: Retorna 0
+ */
+int main(void)
+{
+printf("0123456789\n");
+return (0);
+}
