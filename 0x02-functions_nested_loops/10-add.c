c3d162a65076d4b50dc59985bdeafa087ffd0d91
ssebastiancardona <ssebastiancardona@gmail.com>
2021-02-09 20:53:41 -0500

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - Suma dos Variables como mi ex
+ * @a: primeros cachos
+ * @b: nuevo integrante
+ *
+ * Return: addition of a and b
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
