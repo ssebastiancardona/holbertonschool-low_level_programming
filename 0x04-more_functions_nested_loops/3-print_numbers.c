23b53112d0f0468da9144482bb96355cc5219227
ssebastiancardona <ssebastiancardona@gmail.com>
2021-02-11 21:26:36 -0500

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_numbers - imprime
+ *
+ * Return: no retorna nada
+ */
+void print_numbers(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
