6a2261a7bdba2f223dafe5bf7fba09b3591596ac
ssebastiancardona <ssebastiancardona@gmail.com>
2021-02-11 22:10:03 -0500

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * mul - Multiplica dos variables
+ * @a: Primer variable
+ * @b: Segunda variable
+ *
+ * Return: retorna el resultado de a * b
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
