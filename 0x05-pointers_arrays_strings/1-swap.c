7581d95c6ff8f585aded234c1248076b45949fad
ssebastiancardona <ssebastiancardona@gmail.com>
2021-02-16 22:26:54 -0500

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * swap_int - esta funcion intercambia el valor a y b
+ * @a: a pues es a
+ * @b: y b pues es b
+ *
+ * Return: no retorna nada como 472
+ */
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
