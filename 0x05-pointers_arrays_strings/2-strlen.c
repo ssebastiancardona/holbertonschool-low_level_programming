4882874768a6116574a55b65fedb51d05afbe1f4
ssebastiancardona <ssebastiancardona@gmail.com>
2021-02-16 22:47:55 -0500

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include"holberton.h"
+/**
+ * _strlen - Retorna el tamaño de s
+ * @s: variable para averiguar el tamaño
+ *
+ * Return: retorna la variable longitud
+ */
+int _strlen(char *s)
+{
+int l = strlen(s);
+return (longitud);
+}
