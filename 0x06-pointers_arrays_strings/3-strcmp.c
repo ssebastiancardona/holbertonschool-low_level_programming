e7b9a771a09876e6c17c9a1ea15f656a39e3b6f2
ssebastiancardona <ssebastiancardona@gmail.com>
2021-02-19 18:54:33 -0500

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,9 +7,8 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-for (int i = 0; s1[i] && s2[i] != '\0'; i++)
-{
-return (s1[i] - s2[i]);
+int i = 0;
+while (*(s1 + i) && *(s2 + i) && (*(s1 + i) == *(s2 + i)))
+i++;
+return (*(s1 + i)-*(s2 + i));
 }
-}
-
