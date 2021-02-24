de354fd384ef59e8d122541972a697792d642dff
ssebastiancardona <ssebastiancardona@gmail.com>
2021-02-23 23:20:20 -0500

    9-set_string.c

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * set_string - setea el valor de un puntero a un char
+ *
+ * @s: doble puntero a un string
+ * @to: puntero al string
+ * Return: no retorna nada
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
