eb7e4c1f009e663319f4c402b31870b6f904b6ed
ssebastiancardona <ssebastiancardona@gmail.com>
2021-02-25 23:18:15 -0500

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,3 +1,40 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+/**
+ * main - prints 00-999
+ *
+ * Description: Usa muchos loops
+ * 
+ * Return: retorna 0
+ */
+int main(void)
+{
+int i, j, k;
+i = '0';
+j = i + 1;
+k = j + 1;
+while (i <= '7')
+{
+while (j <= '8')
+{
+while (k <= '9')
+{
+putchar(i);
+putchar(j);
+putchar(k);
+if (i != '7' || j != '8' || k != '9')
+{
+putchar(',');
+putchar(' ');
+}
+k++;
+}
+j++;
+k = j + 1;
+}
+i++;
+j = i + 1;
+k = j + 1;
+}
+putchar('\n');
+return (0);
+}
