6d8a8c1fc16878789568a9b3295c2081e1956425
ssebastiancardona <ssebastiancardona@gmail.com>
2021-02-28 20:26:02 -0500

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - imprime la cantidad de argumentos enviados
+ * @argv: argumentos
+ * @argc: numero de argumentos
+ * Return: nadita ps cero
+ */
+int main(int argc, char *argv[])
+{
+int i;
+num = 0;
+while (num != argc)
+printf("%s\n", argv[i++]);
+return (0);
+}
