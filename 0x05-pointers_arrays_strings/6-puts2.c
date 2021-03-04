91d1516ce65431153dc862ee2cb274101bbd6400
ssebastiancardona <ssebastiancardona@gmail.com>
2021-03-03 23:47:44 -0500

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,39 @@
 #include "holberton.h"
+
 /**
- * puts2 - prints every other character of a  string to stdout
- * @str: string to print
+ * _strlen - returns the length of a given string
  *
+ *
+ * @s: string to check the length of
+ * Return: returns the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+
+/**
+ * imp - prints every other character in a string
+ *
+ * @str: string to print
  * Return: void
  */
-void puts2(char *str)
+
+void imp(char *str)
 {
-int i = 0;
-while (*(str + i))
-{
-_putchar(*(str + i));
-i = i + 2;
-}
-_putchar('\n');
+	int i;
+
+	i = 0;
+	while (i < _strlen(str))
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar(10);
 }
