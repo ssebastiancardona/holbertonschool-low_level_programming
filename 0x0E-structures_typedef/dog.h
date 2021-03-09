44c8b08ed36eacbb580826d857c65992070bd912
ssebastiancardona <ssebastiancardona@gmail.com>
2021-03-08 22:04:39 -0500

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_
+#define _DOG_
+/**
+ * struct dog - listado de pulgas
+ * @name: nombre del bb
+ * @age: edad del bb
+ * @owner: dueño del bb
+ *
+ * Return: nadita
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* _DOG_ */
