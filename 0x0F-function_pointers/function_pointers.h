d7620ce6080e4342dfa1a71c8d743ca836e68488
ssebastiancardona <ssebastiancardona@gmail.com>
2021-03-10 16:34:35 -0500

    function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef _MACRO_
 #define _MACRO_
 
+#include<stdio.h>
+
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
