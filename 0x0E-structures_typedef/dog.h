40b2364b6ab779f8c1e2ca615154ef5a070439a2
ssebastiancardona <ssebastiancardona@gmail.com>
2021-03-10 21:03:08 -0500

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,6 +20,7 @@ struct dog
  */
 typedef struct dog dog_t;
 
+int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void free_dog(dog_t *d);
