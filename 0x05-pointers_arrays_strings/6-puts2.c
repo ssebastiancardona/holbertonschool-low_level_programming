17eb9e6949f63661764f15de74dfb2fa146fea59
ssebastiancardona <ssebastiancardona@gmail.com>
2021-03-10 20:41:44 -0500

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,11 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
-
-	while (*(str + i))
-	{
-		_putchar(*(str + i));
-		i = i + 2;
-	}
-	_putchar('\n');
+int i = 0;
+while (*(str + i))
+{
+_putchar(*(str + i));
+i = i + 2;
+}
+_putchar('\n');
 }
