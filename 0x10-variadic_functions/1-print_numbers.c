32a3bd592ddceb74dea9daecc5e0a7ecdeaacc78
ssebastiancardona <ssebastiancardona@gmail.com>
2021-03-11 19:33:35 -0500

    1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include<stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_numbers - imprimidor de numeros XD
+ * @separator: la coma con el espacio
+ * @n: numero de parametros a imprimir
+ *
+ * Return: nadita
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i;
+	int *s;
+s = 0;
+	va_list parametros;
+va_start(parametros, n);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(parametros, int);
+		printf("%d", s);
+		if (i == n - 1)
+			return;
+		printf("%c ", *separator);
+	}
+	printf("\n");
+	va_end(parametros);
+}
