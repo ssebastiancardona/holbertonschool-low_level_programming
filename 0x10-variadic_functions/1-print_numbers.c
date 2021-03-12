6b0113054fd4cdb6895320c41103241743c7fd66
ssebastiancardona <ssebastiancardona@gmail.com>
2021-03-11 20:43:26 -0500

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ for (i = 0; i < n; i++)
 	s = va_arg(parametros, int);
 printf("%d", s);
 if (i < n - 1 && separator)
-	printf("%s ", separator);
+	printf("%c ", *separator);
 }
 printf("\n");
 va_end(parametros);
