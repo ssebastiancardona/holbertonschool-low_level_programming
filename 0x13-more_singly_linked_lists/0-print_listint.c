73b08bee7008059a0919a973d4044170626fca3e
ssebastiancardona <ssebastiancardona@gmail.com>
2021-03-23 20:11:05 -0500

    correction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,25 +1,20 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * print_list - imprime la lista
- * @h: inicio
- * Return: numero de nodos
+ * print_listint -  imprime toda la lista
+ * @h: lista a imprimir
+ *
+ * Return: numero de elementos
  */
-size_t print_list(const list_t *h)
+size_t print_listint(const listint_t *h)
 {
-	const list_t *tem;
-	size_t nodes = 0;
+	size_t contador = 0;
 
-	tem = h;
-	while (tem != NULL)
+	while (h != NULL)
 	{
-		printf("[%u] ", tem->len);
-		if (!tem->str)
-			printf("(nil)\n");
-		else
-			printf("%s\n", tem->str);
-		tem = tem->next;
-		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+		contador++;
 	}
-	return (nodes);
+	return (contador);
 }
